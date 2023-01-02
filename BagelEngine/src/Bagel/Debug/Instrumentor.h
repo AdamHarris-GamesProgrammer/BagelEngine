479a90f01318f7654b66959e369542ebe4c4119a
Adam Harris <32218978+AdamHarris-GamesProgrammer@users.noreply.github.com>
2023-01-02 14:49:47 +0000

    Optimisations to reduce string creation

--- BagelEngine/src/Bagel/Debug/Instrumentor.h
+++ BagelEngine/src/Bagel/Debug/Instrumentor.h
@@ -24,67 +24,60 @@ namespace Bagel {
 
     class Instrumentor
     {
-    private:
-        InstrumentationSession* m_CurrentSession;
-        std::ofstream m_OutputStream;
-        int m_ProfileCount;
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : _currentSession(nullptr), _profileCount(0)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
-            m_OutputStream.open(filepath);
-            if (m_OutputStream.bad()) {
+            _outputStream.open(filepath);
+            if (_outputStream.bad()) {
                 BG_CORE_ASSERT(false, "Failed to open profiler file")
             }
 
             WriteHeader();
-            m_CurrentSession = new InstrumentationSession{ name };
+            _currentSession = new InstrumentationSession{ name };
         }
 
         void EndSession()
         {
             WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            _outputStream.close();
+            delete _currentSession;
+            _currentSession = nullptr;
+            _profileCount = 0;
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            if (_profileCount++ > 0)
+                _outputStream << ",";
 
             std::string name = result.Name;
             std::replace(name.begin(), name.end(), '"', '\'');
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
+            _outputStream << "{\"cat\":\"function\",\"dur\":";
+            _outputStream << (result.End - result.Start) << ",\"name\":\"";
+            _outputStream << name << "\",\"ph\":\"X\",\"pid\":0,\"tid\":";
+            _outputStream << result.ThreadID << ",\"ts\":";
+            _outputStream << result.Start;
+            _outputStream << "}";
 
-            m_OutputStream.flush();
+            _outputStream.flush();
         }
 
         void WriteHeader()
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-            m_OutputStream.flush();
+            _outputStream << "{\"otherData\": {},\"traceEvents\":[";
+            _outputStream.flush();
         }
 
         void WriteFooter()
         {
-            m_OutputStream << "]}";
-            m_OutputStream.flush();
+            _outputStream << "]}";
+            _outputStream.flush();
         }
 
         static Instrumentor& Get()
@@ -92,20 +85,24 @@ namespace Bagel {
             static Instrumentor instance;
             return instance;
         }
+    private:
+        InstrumentationSession* _currentSession;
+        std::ofstream _outputStream;
+        int _profileCount;
     };
 
     class InstrumentationTimer
     {
     public:
         InstrumentationTimer(const char* name)
-            : m_Name(name), m_Stopped(false)
+            : _pName(name), _isStopped(false)
         {
-            m_StartTimepoint = std::chrono::high_resolution_clock::now();
+            _startTimepoint = std::chrono::high_resolution_clock::now();
         }
 
         ~InstrumentationTimer()
         {
-            if (!m_Stopped)
+            if (!_isStopped)
                 Stop();
         }
 
@@ -113,18 +110,18 @@ namespace Bagel {
         {
             auto endTimepoint = std::chrono::high_resolution_clock::now();
 
-            long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+            long long start = std::chrono::time_point_cast<std::chrono::microseconds>(_startTimepoint).time_since_epoch().count();
             long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
             uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+            Instrumentor::Get().WriteProfile({ _pName, start, end, threadID });
 
-            m_Stopped = true;
+            _isStopped = true;
         }
     private:
-        const char* m_Name;
-        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
-        bool m_Stopped;
+        const char* _pName;
+        std::chrono::time_point<std::chrono::high_resolution_clock> _startTimepoint;
+        bool _isStopped;
     };
 }
 
@@ -142,7 +139,7 @@ namespace Bagel {
 #define BG_PROFILE_SCOPE(name)
 #endif
 
-#define BG_PROFILE_RENDERER 0
+#define BG_PROFILE_RENDERER 1
 
 #if BG_PROFILE_RENDERER
 #define BG_PROFILE_RENDERER_BEGIN_SESSION(name, filepath) BG_PROFILE_BEGIN_SESSION(name,filepath)
